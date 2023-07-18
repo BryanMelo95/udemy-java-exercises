be51d64db52fc9b11610b68e28d84a1dabae99c1
Bryan Melo <bryan.melo@gmail.com>
2023-07-18 15:26:13 -0300

    currency_converter

---
+++ nivelamento-java/src/br/com/udemy/section8/currency_converter/application/Main.java
@@ -0,0 +1,26 @@
+package br.com.udemy.section8.currency_converter.application;
+
+import br.com.udemy.section8.currency_converter.until.CurrencyConverter;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        CurrencyConverter currencyConverter = new CurrencyConverter();
+
+        System.out.print("What is the dollar price? ");
+        currencyConverter.dollarPrice = sc.nextDouble();
+        System.out.println();
+        System.out.print("How many dollars will be bought? ");
+        currencyConverter.dollarsBought = sc.nextDouble();
+        System.out.println();
+
+        System.out.println(currencyConverter);
+    }
+}
---
+++ nivelamento-java/src/br/com/udemy/section8/currency_converter/until/CurrencyConverter.java
@@ -0,0 +1,22 @@
+package br.com.udemy.section8.currency_converter.until;
+
+public class CurrencyConverter {
+
+    public double dollarPrice;
+    public double dollarsBought;
+    public double iof = 0.06;
+
+
+    public double paidInReais() {
+        return dollarsBought * dollarPrice;
+    }
+    public double totalImposto() {
+       return (paidInReais() * iof) + paidInReais();
+    }
+
+    public String toString() {
+        return "Amount to be paid in reais: "
+                + "R$ "
+                + String.format("%.2f", totalImposto());
+    }
+}
